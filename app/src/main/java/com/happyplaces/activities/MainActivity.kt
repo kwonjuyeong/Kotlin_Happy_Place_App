481092c4d24ba6908b573f9bed637871a9b58c00
kwonjuyoung <20161527@vision.hoseo.edu>
2022-04-03 20:16:59 +0900

    Local DB 사용

--- app/src/main/java/com/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/happyplaces/activities/MainActivity.kt
@@ -2,8 +2,11 @@ package com.happyplaces.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.happyplaces.R
+import com.happyplaces.database.DatabaseHandler
+import com.happyplaces.models.HappyPlaceModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +18,21 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, AddHappyPlaceActivity::class.java)
             startActivity(intent)
         }
+        getHappyPlacesListFromLocalDB()
         // END
+    }
+
+    private fun getHappyPlacesListFromLocalDB(){
+        val dbHandler = DatabaseHandler(this)
+        val getHappyPlaceList : ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
+
+        if(getHappyPlaceList.size>0)
+            for(i in getHappyPlaceList){
+
+                //등록했던 Happyplace 중 Title, Description부분만 로컬에 저장해서 로그에 띄어주는 테스트해봄.
+                Log.e("Title", i.title)
+                Log.e("Description", i.description)
+            }
+
     }
 }
--- app/src/main/java/com/happyplaces/database/DatabaseHandler.kt
+++ app/src/main/java/com/happyplaces/database/DatabaseHandler.kt
@@ -2,7 +2,9 @@ package com.happyplaces.database
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
 import android.database.sqlite.SQLiteOpenHelper
 import com.happyplaces.models.HappyPlaceModel
 
@@ -63,9 +65,38 @@ class DatabaseHandler(context: Context) :
         val result = db.insert(TABLE_HAPPY_PLACE, null, contentValues)
         //2nd argument is String containing nullColumnHack  
 
-        db.close() // Closing database connection
+        db.close()
         return result
     }
-    // END
+
+    fun getHappyPlacesList():ArrayList<HappyPlaceModel>{
+        val happyPlaceList = ArrayList<HappyPlaceModel>()
+        val selectQuery = "SELECT *FROM $TABLE_HAPPY_PLACE"
+        val db = this.readableDatabase
+
+        try{
+            val cursor : Cursor = db.rawQuery(selectQuery, null)
+            if(cursor.moveToFirst()){
+                do{
+                    val place = HappyPlaceModel(
+                        cursor.getInt(cursor.getColumnIndex(KEY_ID)),
+                        cursor.getString(cursor.getColumnIndex(KEY_TITLE)),
+                        cursor.getString(cursor.getColumnIndex(KEY_IMAGE)),
+                        cursor.getString(cursor.getColumnIndex(KEY_DESCRIPTION)),
+                        cursor.getString(cursor.getColumnIndex(KEY_DATE)),
+                        cursor.getString(cursor.getColumnIndex(KEY_LOCATION)),
+                        cursor.getDouble(cursor.getColumnIndex(KEY_LATITUDE)),
+                        cursor.getDouble(cursor.getColumnIndex(KEY_LONGITUDE))
+                    )
+                    happyPlaceList.add(place)
+
+                }while(cursor.moveToNext())
+            }
+            cursor.close()
+        }catch (e: SQLiteException){
+            db.execSQL(selectQuery)
+            return ArrayList()
+        }
+        return happyPlaceList
+    }
 }
-// END
